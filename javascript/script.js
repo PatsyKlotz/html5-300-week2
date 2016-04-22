af0572ac9d55b5fad1e61fe847dc1a6e2833d169
Patsyklotz <patsy.neher@gmail.com>
2016-04-21 17:05:59 -0700

    new tests

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,27 @@
 //create an array variable to hold data
 
 
-/*var worldLeaders = ["Gandi", "Nelson Mandela", "Martin Luther King, Jr.", "Winston Churchill", "Mother Teresa"];
-document.getElementById("t01").innerHTML = worldLeaders;*/
+var worldLeaders = ["Gandi", "Nelson Mandela", "Martin Luther King, Jr.", "Winston Churchill", "Mother Teresa"];
+document.getElementById("emptytable").innerHTML = "worldLeaders";
   
   
 
 //write a function that creates a table row for each object in the array, with a cell for each property to be displayed. Append each of the rows to the table body.
 
-$(document).ready(function(){
+/*$(document).ready(function(){
+
+function myFunction() {
+  
+  var table = document.getElementById=("t01");
+  var row = table.insertRow(0);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  document.getElementById("emptytable").innerHTML=("worldLeaders");
+  
+  }});*/
+  
+
+/*$(document).ready(function(){
   
    function myFunction() {
     var x = document.createElement("TR");
@@ -21,7 +34,7 @@ $(document).ready(function(){
     document.getElementById("myTr").appendChild(y);
 }
 
-});
+});*/
                   
 //Write an event handler for the new-item button. It should hide the table and show the form.
   
